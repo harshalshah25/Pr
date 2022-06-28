d11f67092f3df4618c3ca5ea3417dc30b63ffbf6
Harshal Shah <shahharshal316@gmail.com>
2022-06-28 18:33:37 +0530

    Third commit

--- BinaryR.java
+++ BinaryR.java
@@ -2,7 +2,7 @@ public class BinaryR {
    
         public static void main(String[] arge){
     
-            int[] arr ={1,2,3,4,5,6,7,8,9};
+            int[] arr ={1,2,3,4,5,6,7,8,9,10};
             int target =6;
             int start =0;
             int end = arr.length-1;
